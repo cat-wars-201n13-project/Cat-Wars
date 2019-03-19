c075ad98c4aeecae63dba176a836564720c03031
rebecca-pete <rebecca@rebeccapeterson.us>
2019-03-18 19:54:52 -0700

    BeckyV8 adding breed values to neighborhood select box

--- js/game.js
+++ js/game.js
@@ -34,8 +34,16 @@ console.log('Ballard: ', Ballard);
 console.log('Queen Anne: ', QueenAnne);
 console.log('Downtown: ', Downtown);
 
+//for in loop to check all properties in the Ballard Neighborhood object
+
 function breedValueByNeighborhood() {
-	var x = Ballard.breedsValue.tabby[1];
+  var x = Ballard.breedsValue.tabby[1];
+  var y = Ballard.breedsValue.calico[1];
+  var z = Ballard.breedsValue.mainecoon[1];
+  var xx = Ballard.breedsValue.persian[1];
+  var yy = Ballard.breedsValue.british[1];
+  var zz = Ballard.breedsValue.siamese[1];
+
 	console.log('This is breed value', x);
 	x = JSON.stringify(x);
 	console.log(x);
@@ -44,7 +52,43 @@ function breedValueByNeighborhood() {
   var tdEl = document.createElement('td'); //make a td cell
   tdEl.textContent = ' ' + x; //add text to the td cell you just created
   trEl.appendChild(tdEl); //append the td cell to the tr row
-//start here
+
+	y = JSON.stringify(y);
+ 
+  var trEl = document.getElementById('optionCalico'); //get the option row
+  var tdEl = document.createElement('td'); //make a td cell
+  tdEl.textContent = ' ' + y; //add text to the td cell you just created
+  trEl.appendChild(tdEl); //append the td cell to the tr row
+
+  z = JSON.stringify(z);
+ 
+  var trEl = document.getElementById('optionMainecoon'); //get the option row
+  var tdEl = document.createElement('td'); //make a td cell
+  tdEl.textContent = ' ' + z; //add text to the td cell you just created
+  trEl.appendChild(tdEl); //append the td cell to the tr row
+
+  xx = JSON.stringify(xx);
+ 
+  var trEl = document.getElementById('optionPersian'); //get the option row
+  var tdEl = document.createElement('td'); //make a td cell
+  tdEl.textContent = ' ' + xx; //add text to the td cell you just created
+  trEl.appendChild(tdEl); //append the td cell to the tr row
+
+  console.log('This is breed value', yy);
+	yy = JSON.stringify(yy);
+	console.log(yy);
+ 
+  var trEl = document.getElementById('optionBritish'); //get the option row
+  var tdEl = document.createElement('td'); //make a td cell
+  tdEl.textContent = ' ' + yy; //add text to the td cell you just created
+  trEl.appendChild(tdEl); //append the td cell to the tr row
+
+  zz = JSON.stringify(zz);
+ 
+  var trEl = document.getElementById('optionSiamese'); //get the option row
+  var tdEl = document.createElement('td'); //make a td cell
+  tdEl.textContent = ' ' + zz; //add text to the td cell you just created
+  trEl.appendChild(tdEl); //append the td cell to the tr row
 }
 breedValueByNeighborhood();
 
