455a1e80c811050248866074dde3347c93e5f97f
rebecca-pete <rebecca@rebeccapeterson.us>
2019-03-18 21:26:38 -0700

    code to display cat breed values for all neighborhoods, once event listener is added to change neighborhood button

--- js/game.js
+++ js/game.js
@@ -21,12 +21,13 @@ function Neighborhood(name, tabby, calico, mainecoon, persian, british, siamese)
   this.breedsValue = {
     tabby: ['Tabby', tabby], // 200
     calico: ['Calico', calico], // 250
-    mainecoon: ['Mainecoon', mainecoon], // 300 
+    mainecoon: ['Mainecoon', mainecoon], // 300
     persian: ['Persian', persian], // 400
-    british: ['British' + british], // 500
-    siamese: ['Siamese' + siamese] // 600
+    british: ['British', british], // 500
+    siamese: ['Siamese', siamese] // 600
   };
 }
+
 var Ballard = new Neighborhood('Ballard', 200, 250, 300, 400, 500, 600);
 var QueenAnne = new Neighborhood('QueenAnne', 100, 300, 800, 500, 1200, 75);
 var Downtown = new Neighborhood('Downtown', 75, 725, 100, 300, 1400, 1500);
@@ -34,74 +35,46 @@ console.log('Ballard: ', Ballard);
 console.log('Queen Anne: ', QueenAnne);
 console.log('Downtown: ', Downtown);
 
+var arrayOfNeighborhoods = [Ballard, QueenAnne, Downtown];
+var catBreeds = ['Tabby', 'Calico', 'Mainecoon', 'Persian', 'British', 'Siamese'];
+var catPrices = [0, 0, 0, 0, 0, 0];
+
 //for in loop to check all properties in the Ballard Neighborhood object
 
-function breedValueByNeighborhood() {
-  var x = Ballard.breedsValue.tabby[1];
-  var y = Ballard.breedsValue.calico[1];
-  var z = Ballard.breedsValue.mainecoon[1];
-  var xx = Ballard.breedsValue.persian[1];
-  var yy = Ballard.breedsValue.british[1];
-  var zz = Ballard.breedsValue.siamese[1];
+function breedValueByNeighborhood(arrayOfNeighborhoodsIndex) {
 
-	console.log('This is breed value', x);
-	x = JSON.stringify(x);
-	console.log(x);
-  // document.getElementById('Tabby Value').innerHTML = x; //not displaying
-  var trEl = document.getElementById('optionTabby'); //get the option row
-  var tdEl = document.createElement('td'); //make a td cell
-  tdEl.textContent = ' ' + x; //add text to the td cell you just created
-  trEl.appendChild(tdEl); //append the td cell to the tr row
+  catPrices[0] = arrayOfNeighborhoods[arrayOfNeighborhoodsIndex].breedsValue.tabby[1];
+  catPrices[1] = arrayOfNeighborhoods[arrayOfNeighborhoodsIndex].breedsValue.calico[1];
+  catPrices[2] = arrayOfNeighborhoods[arrayOfNeighborhoodsIndex].breedsValue.mainecoon[1];
+  catPrices[3] = arrayOfNeighborhoods[arrayOfNeighborhoodsIndex].breedsValue.persian[1];
+  catPrices[4] = arrayOfNeighborhoods[arrayOfNeighborhoodsIndex].breedsValue.british[1];
+  catPrices[5] = arrayOfNeighborhoods[arrayOfNeighborhoodsIndex].breedsValue.siamese[1];
 
-	y = JSON.stringify(y);
- 
-  var trEl = document.getElementById('optionCalico'); //get the option row
-  var tdEl = document.createElement('td'); //make a td cell
-  tdEl.textContent = ' ' + y; //add text to the td cell you just created
-  trEl.appendChild(tdEl); //append the td cell to the tr row
-
-  z = JSON.stringify(z);
- 
-  var trEl = document.getElementById('optionMainecoon'); //get the option row
-  var tdEl = document.createElement('td'); //make a td cell
-  tdEl.textContent = ' ' + z; //add text to the td cell you just created
-  trEl.appendChild(tdEl); //append the td cell to the tr row
-
-  xx = JSON.stringify(xx);
- 
-  var trEl = document.getElementById('optionPersian'); //get the option row
-  var tdEl = document.createElement('td'); //make a td cell
-  tdEl.textContent = ' ' + xx; //add text to the td cell you just created
-  trEl.appendChild(tdEl); //append the td cell to the tr row
-
-  console.log('This is breed value', yy);
-	yy = JSON.stringify(yy);
-	console.log(yy);
- 
-  var trEl = document.getElementById('optionBritish'); //get the option row
-  var tdEl = document.createElement('td'); //make a td cell
-  tdEl.textContent = ' ' + yy; //add text to the td cell you just created
-  trEl.appendChild(tdEl); //append the td cell to the tr row
-
-  zz = JSON.stringify(zz);
- 
-  var trEl = document.getElementById('optionSiamese'); //get the option row
-  var tdEl = document.createElement('td'); //make a td cell
-  tdEl.textContent = ' ' + zz; //add text to the td cell you just created
-  trEl.appendChild(tdEl); //append the td cell to the tr row
+  for (var i = 0; i < catBreeds.length; i++) {
+    // debugger;
+    var trEl = document.getElementById('option' + catBreeds[i]); //get the option row
+    if(document.getElementById(catBreeds[i] + 'Price')) {
+      tdEl = document.getElementById(catBreeds[i] + 'Price');
+    } else {
+      var tdEl = document.createElement('td'); //make a td cell
+      tdEl.id = catBreeds[i] + 'Price';
+    }
+    tdEl.textContent = ' ' + catPrices[i]; //add text to the td cell you just created
+    trEl.appendChild(tdEl); //append the td cell to the tr row
+  }
 }
-breedValueByNeighborhood();
+breedValueByNeighborhood(0);
 
 function userPurse() {
-	var x = Bud.purse;
-	console.log(x);
+  var x = Bud.purse;
+  console.log(x);
   document.getElementById('purse').innerHTML = x;
 }
 function userInventory() {
-	var x = Bud.catInventory;
-	console.log(x);
-	x = JSON.stringify(x);
-	console.log(x);
+  var x = Bud.catInventory;
+  console.log(x);
+  x = JSON.stringify(x);
+  console.log(x);
   document.getElementById('inventory').innerHTML = x;
 }
 
@@ -114,8 +87,8 @@ function buyCat() {
   console.log(cost);
   Bud.purse = Bud.purse - cost;
   console.log('Bud.purse: ', Bud.purse);
-	userPurse();
-	userInventory();
+  userPurse();
+  userInventory();
 }
 
 function sellCat() {
@@ -127,8 +100,8 @@ function sellCat() {
   console.log(cost);
   Bud.purse = Bud.purse + cost;
   console.log('Bud.purse: ', Bud.purse);
-	userPurse();
-	userInventory();
+  userPurse();
+  userInventory();
 }
 
 var buyButton = document.getElementById('buycat');
