2dc9aec3383cea90427a7676b72a82050ca268bf
rebecca-pete <rebecca@rebeccapeterson.us>
2019-03-19 21:58:15 -0700

    Inventory select box now displays the user inventory. Scoot button added to allow user to change neighborhoods. Hard-coded text replaced with code for dynamic code.

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@
 function User(username, purse) {
   this.purse = purse;
   this.username = username;
+  this.neighborhood = 0;
   this.catInventory = {
     tabby: 0,
     calico: 0,
@@ -12,8 +13,8 @@ function User(username, purse) {
     siamese: 0,
   };
 }
-var Bud = new User('Bud', 500);
-console.log('Bud', Bud);
+var Player = new User('Mike', 500);
+console.log('Player', Player);
 userPurse();
 
 function Neighborhood(name, tabby, calico, mainecoon, persian, british, siamese) {
@@ -31,6 +32,7 @@ function Neighborhood(name, tabby, calico, mainecoon, persian, british, siamese)
 var Ballard = new Neighborhood('Ballard', 200, 250, 300, 400, 500, 600);
 var QueenAnne = new Neighborhood('QueenAnne', 100, 300, 800, 500, 1200, 75);
 var Downtown = new Neighborhood('Downtown', 75, 725, 100, 300, 1400, 1500);
+
 console.log('Ballard: ', Ballard);
 console.log('Queen Anne: ', QueenAnne);
 console.log('Downtown: ', Downtown);
@@ -61,53 +63,96 @@ function breedValueByNeighborhood(arrayOfNeighborhoodsIndex) {
     }
     tdEl.textContent = ' ' + catPrices[i]; //add text to the td cell you just created
     trEl.appendChild(tdEl); //append the td cell to the tr row
+
+    var header = document.getElementById('neighborhood');
+    header.textContent = arrayOfNeighborhoods[Player.neighborhood].name;
   }
 }
-breedValueByNeighborhood(0);
+breedValueByNeighborhood(Player.neighborhood);
 
+
+function breedQuantityToSellByUser(Player) {
+  for (var i = 0; i < catBreeds.length; i++) { //how do I check all properties in the object?
+  
+    var trEl = document.getElementById(catBreeds[i] + 'Row'); //get the option row
+    if(document.getElementById(catBreeds[i] + 'Quantity')) {
+      tdEl = document.getElementById(catBreeds[i] + 'Quantity');
+    } else {
+      var tdEl = document.createElement('td'); //make a td cell
+
+      tdEl.id = catBreeds[i] + 'Quantity';
+    }
+    tdEl.textContent = ' ' + Player.catInventory[catBreeds[i].toLowerCase()]; //add text to the td cell you just created //start here
+    trEl.appendChild(tdEl); //append the td cell to the tr row
+  }
+}
+breedQuantityToSellByUser(Player);
+
+//rewrite dynamically, do last
 function userPurse() {
-  var x = Bud.purse;
+  var x = Player.purse;
   console.log(x);
   document.getElementById('purse').innerHTML = x;
 }
+//rewrite dynamically, do last
 function userInventory() {
-  var x = Bud.catInventory;
+  var x = Player.catInventory;
   console.log(x);
   x = JSON.stringify(x);
   console.log(x);
   document.getElementById('inventory').innerHTML = x;
 }
 
-
-//start here; check str and .value
 function buyCat() {
-  var str = document.getElementById('selectBreed').value; //why not event.target.value?
-  str = str.toLowerCase();
-  console.log('User chose:', str); // str is defined as "tabby 200" or all of the value to the key on line 22
-  Bud.catInventory[str] += 1;
-  str = str.charAt(0).toUpperCase() + str.slice(1); //slices the integer from the breed string
+  //var str = document.getElementById('selectBreed').value; //why not event.target.value?
+  var str = document.getElementById('selectBreed').value; //how do I separate breed from value?
+  str = str.toLowerCase(); 
+  console.log('User chose:', str); // str is defined as "tabby 200" 
+
+  var breedName = str.split(' ')[0];
+  var cost = +(str.split(' ')[1]);
+  Player.catInventory[breedName.toLowerCase()] += 1;
   //var cost = Ballard.breedsValue[str][1];
   //if , need random function
-  var cost = arrayOfNeighborhoods[0].breedsValue.str[1]; //why won't this work?
+  //var cost = arrayOfNeighborhoods[0].breedsValue[amount]; 
   console.log(cost);
-  Bud.purse = Bud.purse - cost;
-  console.log('Bud.purse: ', Bud.purse);
+  Player.purse = Player.purse - cost;
+  console.log('Player.purse: ', Player.purse);
   userPurse();
   userInventory();
+  breedQuantityToSellByUser(Player);
 }
 
 function sellCat() {
-  var str = document.getElementById('selectBreedToSell').value; //using the str variable as not a string
-  str = str.toLowerCase(); //try making this a unique variable so that I can use the str variable as a string
+  var str = document.getElementById('selectBreedToSell').value; 
+  str = str.toLowerCase(); 
   console.log('User chose:', str);
-  Bud.catInventory[str] -= 1;
-  var cost = Ballard.breedsValue[str][1];
-  //var cost = arrayOfNeighborhoods[0].breedsValue[str][1]; //why won't this work?
-  console.log(cost);
-  Bud.purse = Bud.purse + cost;
-  console.log('Bud.purse: ', Bud.purse);
-  userPurse();
-  userInventory();
+ 
+  var breedName = str.split(' ')[0];
+  var quantityCats = +(str.split(' ')[1]);
+  if(quantityCats > 0) {
+    Player.catInventory[breedName] -= 1;
+   
+    var cost = arrayOfNeighborhoods[Player.neighborhood].breedsValue[breedName][1];
+    // str = str.charAt(0).toUpperCase() + str.slice(1);
+
+    console.log(cost);
+    Player.purse = Player.purse + cost;
+    console.log('Player.purse: ', Player.purse);
+    userPurse();
+    userInventory();
+    breedQuantityToSellByUser(Player);
+  }
+}
+
+function changeNeighborhood() {
+  var randomNeighborhood = Math.floor(Math.random() * arrayOfNeighborhoods.length);
+  while (randomNeighborhood === Player.neighborhood) {
+    randomNeighborhood = Math.floor(Math.random() * arrayOfNeighborhoods.length);
+  }
+  Player.neighborhood = randomNeighborhood;
+  breedValueByNeighborhood(Player.neighborhood);
+  breedQuantityToSellByUser(Player);
 }
 
 var buyButton = document.getElementById('buycat');
@@ -115,3 +160,6 @@ buyButton.addEventListener('click', buyCat);
 
 var sellButton = document.getElementById('sellcat');
 sellButton.addEventListener('click', sellCat);
+
+var scootButton = document.getElementById('changeNeighborhood');
+scootButton.addEventListener('click', changeNeighborhood);
